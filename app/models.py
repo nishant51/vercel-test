40c93e7f1a487d65527b4c4f811b03261d9a8360
nishant <timsinanishant51@gmail.com>
2024-03-06 20:34:42 +0545

    changes made okay

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Event(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now_add=True)
-    image = models.BinaryField(null=True, blank=True)  # Use BinaryField to store binary data
+    image = models.ImageField(null=True, blank=True)
     body = models.TextField(null=True, blank=True)
 
     def __str__(self):
--- app/serializers.py
+++ app/serializers.py
@@ -7,9 +7,3 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['id', 'title', 'date', 'image', 'image_url', 'body']
-
-    def get_image_url(self, obj):
-        if obj.image:
-            # Assuming MEDIA_URL is '/media/' and obj.image.url is the relative path of the image
-            return self.context['request'].build_absolute_uri(obj.image.url)
-        return None
